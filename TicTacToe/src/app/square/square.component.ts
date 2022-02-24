5daefb8cd7f68d16178970732a84dc4cd4b43fc5
andreibuiciuc <abuiciuc33@gmail.com>
2022-02-24 21:32:13 +0200

    add square component

---
+++ TicTacToe/src/app/square/square.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'square',
+  template: `
+    <button> {{ value }} </button>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {  // UI dumb component
+  @Input() value: 'X' | 'O' | undefined;
+}
